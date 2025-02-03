db599c0760e3a156a0cd6254c29c7dc9e67bbf23
ivan-uroz <160120456+ivan-uroz@users.noreply.github.com>
2025-02-03 18:31:32 +0100

    Update pg_backup.sh

--- ingesta-BO/backup/pg_backup.sh
+++ ingesta-BO/backup/pg_backup.sh
@@ -2,4 +2,4 @@
 
 # credenciales
 source /app/.env
-pg_dump --dbname=postgresql://$DB_USR:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME > backup.sql
+pg_dump --dbname=postgresql://$DB_EMPLEO_USER:$DB_EMPLEO_PASS@$BACK_HOST:$DB_EMPLEO_PORT/$DB_EMPLEO_NAME > backup.sql
